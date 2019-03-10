4182135f655b04cfa8742813a65682cff867b811
chenyuqin <chenyuqin1024@163.com>
2019-03-10 09:07:46 +0800

    done

--- src/main/java/com/book/BookAdminApplication.java
+++ src/main/java/com/book/BookAdminApplication.java
@@ -3,13 +3,22 @@ package com.book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.book.mapper")
 @SpringBootApplication
-public class BookAdminApplication {
+public class BookAdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BookAdminApplication.class, args);
+    }
+
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
